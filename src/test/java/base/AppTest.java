7827bf0f8513899e02a594c591f61be170cd1e13
Aleksandr Shevkunenko <v-ashevkunenko@corp.epnet.com>
2018-12-03 11:49:21 +0300

    Initial commit

---
+++ src/test/java/base/AppTest.java
@@ -0,0 +1,12 @@
+package base;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void someTest() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
